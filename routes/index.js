f8087d26494eefb8d041d93a802248d3d0ee7528
Simon Orzel <simon.orzel@dynabase.de>
2022-03-29 18:56:24 +0200

    feat

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,20 @@
 var express = require("express");
 var router = express.Router();
-var axios = require("axios");
+var puppeteer = require("puppeteer");
 
 router.get("/", async function (req, res, next) {
-  const response = await axios.post(
-    "https://www.facebook.com/api/graphql?variables=%7B%22pageID%22%3A%22123137871031991%22%7D&doc_id=5182274978466320",
-    null,
-    {
-      headers: {
-        "content-type": "application/x-www-form-urlencoded",
-        "Accept-Language": "en-US,en;",
-        accept: "*/*",
-        origin: "https://www.facebook.com/",
-        referer:
-          "https://www.facebook.com/pg/kizombabisumadrid/events/?ref=page_internal&mt_nav=0",
-      },
-    }
-  );
-  res.send(response.data);
+  const browser = await puppeteer.launch();
+  const [page] = await browser.pages();
+
+  await page.goto("https://m.facebook.com/pg/moodclubpalermo/events/", {
+    waitUntil: "networkidle0",
+  });
+  const data = await page.evaluate(() => document.querySelector("*").outerHTML);
+
+  console.log(data);
+
+  await browser.close();
+  res.send(data);
 });
 
 module.exports = router;
